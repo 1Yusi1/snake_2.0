7cac9bcbce4ca988f40f595965abdad855c5bd7d
1Yusi1 <tatiana.kokurina@gmail.com>
2025-07-15 21:46:16 +0300

    Rewritten for OOP

--- snake_2.0.cpp
+++ snake_2.0.cpp
@@ -8,199 +8,280 @@
 using namespace std;
 using namespace std::chrono;
 
-bool game_exit = false;
+//шаблонный класс для рисования объектов
 
 struct vector2d {
-    int x = 0; 
+    int x = 0;
     int y = 0;
 };
 
-void showMap(const vector2d &apple, const int &eaten_apple) {
-    clear();
-    move(2, 55);
+bool game_exit = false;
 
-    attrset(A_DIM | COLOR_PAIR(1)); //устанавливаются атрибуты для текста далее
-    printw("Змейка\t\t");
-    attrset(A_BOLD | COLOR_PAIR(5));
-    printw("Количество собранных яблок\t\t");
-    printw(to_string(eaten_apple).c_str());
-    printw(" > ");
+class Apple {
+public:
+    Apple() {}
 
-    //Border (Граница)
-    attrset(A_DIM | COLOR_PAIR(1));
-    for (auto& value : { 4, 27 }) {
-        for (int x = 5; x < 112; x++) {
-            move(value, x);
-            printw("*");
-        }
+    Apple(const vector2d startCords, int color) : cords(startCords), numberOfColorPair(color) {}
+
+    void drawInTheMap() {
+        move(cords.x, cords.y);
+        attrset(A_BOLD | COLOR_PAIR(numberOfColorPair));
+        printw("@");
     }
 
-    for (auto& value : { 5, 112 }) {
-        for (int y = 4; y < 28; y++) {
-            move(y, value);
-            printw("*");
-        }
+    vector2d getCords() {
+        return cords;
     }
 
-    move(apple.y, apple.x);
-    attrset(A_BOLD | COLOR_PAIR(2));
-    printw("@");
-
-
-}
-
-void moveSnake(vector2d& snake_head, vector2d& step, vector<vector2d>& tail, vector2d& apple, vector2d& rand_apple, int& eaten_apples) {
-    
-    attrset(A_BOLD | COLOR_PAIR(3)); // цвет змейки
-
-    if (!tail.empty()) {
-        for (const auto& part : tail) {
-            move(part.x, part.y);
-            printw("#");
-        }
+    void setCords(vector2d value) {
+        cords.x = value.x;
+        cords.y = value.y;
+        //cords = value;
     }
 
-    //Координаты головы змеи
-    snake_head.x += step.x;
-    snake_head.y += step.y;
+    static vector2d getRandomCords() {
+        default_random_engine rand(static_cast<unsigned int>(system_clock::now().time_since_epoch().count()));
+        uniform_int_distribution<int> apple_x(4, 26);
+        uniform_int_distribution<int> apple_y(5, 109);
 
-    move(snake_head.x, snake_head.y);
-    auto s = static_cast<char>(winch(stdscr));
-    if (s == '*' || s == '#') {
-        attrset(A_BOLD | COLOR_PAIR(4));
-        move(13, 55);
-        printw("Конец игры");
-        move(14, 42);
-        printw("Выход - < q >  Начать заново - < n >");
-        // *** Цикл выбора игрового меню ***
-        do {
-            if (getch() == 'q') {
-                // выход из игры
-                game_exit = true;
-                return;
-            }
-            if (getch() == 'n')
-            {
-                // рестарт задаём начальные координаты переменным
-                snake_head = { 10,10 };
-                step = { 0, 1 };
-                tail.clear();
-                apple = { 15,15 };
-                eaten_apples = 0;
-                return;
-            }
-        } while (true);
-        // *** Конец цикла выбора игрового меню ***
 
+        return {apple_x(rand), apple_y(rand)};
     }
-    
-    if (s == '@') {
-        eaten_apples++;
-        tail.push_back({snake_head.x, snake_head.y});
-        printw("$");
 
-        do {
-            apple.x = rand_apple.x;
-            apple.y = rand_apple.y;
-            move(apple.y, apple.x);
-            auto s = static_cast<char>(winch(stdscr));
-            
-        } while (s == '#');
+    Apple(const Apple& _apple) {
+        this->cords.x = _apple.cords.x;
+        this->cords.y = _apple.cords.y;
+        this->numberOfColorPair = _apple.numberOfColorPair;
     }
-    else {
-        printw("$");
-        //update tail snake
+
+private:
+    vector2d cords;
+    int numberOfColorPair;
+
+};
+
+class Snake {
+public:
+    Snake() {}
+
+    //rewrite (must work base)
+    Snake(const vector2d startCords, const Apple& _apple) {
+        head.x = startCords.x;
+        head.y = startCords.y;
+        this->apple = _apple;
+        
+    }
+    void next() {
+        attrset(A_BOLD | COLOR_PAIR(3));
+
         if (!tail.empty()) {
-            tail.erase(tail.begin());
-            tail.push_back({ snake_head.x, snake_head.y });
+            for (const auto& part : tail) {
+                move(part.x, part.y);
+                printw("#");
+            }
         }
+
+        head.x += step.x; // operator +=
+        head.y += step.y;
+
+        move(head.x, head.y);
+        auto s = static_cast<char>(winch(stdscr));
+
+        if (s == '*' || s == '#') {
+            attrset(A_BOLD | COLOR_PAIR(4));
+            move(13, 55);
+            printw("Конец игры");
+            move(14, 42);
+            printw("Выход - < q >  Начать заново - < n >");
+
+            do {
+                if (getch() == 'q') {
+                    game_exit = true;
+                    return;
+                    
+                }
+                if (getch() == 'n') {
+                    //начальные значения
+                    head = { 10,10 };
+                    step = { 0, 1 };
+                    tail.clear();
+                    apple.setCords({15, 15});
+                    eaten_apples = 0;
+                    
+                }
+            
+            } while (true);
+
+        }
+        if (s == '@') {
+            eaten_apples++;
+            tail.push_back({ head.x, head.y });
+            do {
+                apple.setCords(Apple::getRandomCords());
+                move(apple.getCords().x, apple.getCords().y);
+                auto s = static_cast<char>(winch(stdscr));
+            
+            } while (s == '#');
+        
+        }
+        else {
+            printw("$");
+            //update tail snake
+            if (!tail.empty()) {
+                tail.erase(tail.begin());
+                tail.push_back({ head.x, head.y });
+            }    
+        }
+        
     }
 
-}
+    int getEatenApples() { return eaten_apples; }
 
-void startingSettings() {
+    vector2d getStep() {
+        return this->step;
+    }
+
+    void setStep(vector2d value) {
+        step.x = value.x;
+        step.y = value.y;
+    }
+
+    Apple& getApple() {
+        return apple;
+    }
+
+    void drawInTheMap() {
+        attrset(A_BOLD | COLOR_PAIR(3));
+
+        if (!tail.empty()) {
+            for (const auto& part : tail) {
+                move(part.x, part.y);
+                printw("#");
+            }
+        } 
+
+        move(head.x, head.y);
+        printw("$");
+
+    }
+
+private:
+    vector2d head = {10, 10};
+    vector<vector2d> tail;
+    vector2d step = { 0, 1 }; // x, y
+    Apple apple;
+    int eaten_apples = 0;
+
+};
+
+class Game {
+public:
+    Game(int numberOfPlayer) : snake({ 10, 10 }, Apple({ 10, 15 }, 2)) {
+        applySettings();
+    }
+
+    void start() {  //стрелочки и бесконечный цикл
+        while (!game_exit) {
+            updateFrame();
+            snake.next();
+            timeout(170);
+            switch (getch()) {
+            case KEY_UP:
+                if (snake.getStep().x == 0) {
+                    snake.setStep({ -1, 0 });
+
+                }
+                break;
+            case KEY_DOWN:
+                if (snake.getStep().x == 0) {
+                    snake.setStep({ 1, 0 });
+
+                }
+                break;
+            case KEY_LEFT:
+                if (snake.getStep().y == 0) {
+                    snake.setStep({ 0, -1 });
+
+                }
+                break;
+            case KEY_RIGHT:
+                if (snake.getStep().y == 0) {
+                    snake.setStep({ 0, 1 });
+                }
+                break;
+            case 'q':
+                game_exit = true;
+                break;
+            }
+        }
 
 
-}
+    
+    }
+
+    void updateFrame() {
+        clear();
+        move(2, 55);
+
+        attrset(A_DIM | COLOR_PAIR(1)); //устанавливаются атрибуты для текста далее
+        printw("Змейка\t\t");
+        attrset(A_BOLD | COLOR_PAIR(5));
+        printw("Количество собранных яблок\t\t");
+        printw(to_string(snake.getEatenApples()).c_str());
+        printw(" > ");
+
+        //Border (Граница)
+        attrset(A_DIM | COLOR_PAIR(1));
+        for (auto& value : { 4, 27 }) {
+            for (int x = 5; x < 112; x++) {
+                move(value, x);
+                printw("*");
+            }
+        }
+
+        for (auto& value : { 5, 112 }) {
+            for (int y = 4; y < 28; y++) {
+                move(y, value);
+                printw("*");
+            }
+        }
+
+        snake.getApple().drawInTheMap();
+    }
+
+    ~Game() {
+        endwin();
+    }
+
+
+private:
+    void applySettings() {
+        system("chcp 1251");
+
+        initscr();
+        curs_set(0);
+        start_color();
+
+        init_pair(1, COLOR_WHITE, COLOR_BLUE);  //background
+        init_pair(2, COLOR_MAGENTA, COLOR_BLUE); //apple
+        init_pair(3, COLOR_GREEN, COLOR_BLUE);  //snake
+        init_pair(4, COLOR_RED, COLOR_BLUE);    //endgame
+        init_pair(5, COLOR_YELLOW, COLOR_BLACK);  //text
+
+        bkgd(COLOR_PAIR(1));
+
+        keypad(stdscr, true); //возможность использовать клавиатуру
+        noecho();
+    }
+    Snake snake;
+    //int colorBkgd;  //потом попробовать
+};
 
 int main()
 {
-    system("chcp 1251");
+    Game game(1);
+    game.start();
 
-    initscr();
-    curs_set(0);
-    start_color();
 
-    init_pair(1, COLOR_WHITE, COLOR_BLUE);
-    init_pair(2, COLOR_MAGENTA ,COLOR_BLUE);
-    init_pair(3, COLOR_GREEN, COLOR_BLUE);
-    init_pair(4, COLOR_RED, COLOR_BLUE);
-    init_pair(5, COLOR_YELLOW, COLOR_BLACK);
-
-    bkgd(COLOR_PAIR(1));
-
-    vector2d snake_head{ 10, 10 };
-    vector2d vector_step{ 0, 1 };
-    vector2d apple{ 15, 15 };
-    vector2d rand_apple;
-    vector<vector2d> snake_tail;
-
-    int frame_rate = 500;
-    int eaten_apples = 0;
-
-    //Генератор случайных чисел 
-    long long seed = system_clock::now().time_since_epoch().count();
-    default_random_engine rnd(static_cast<unsigned int>(seed));
-    uniform_int_distribution<int> apple_x(6, 99);
-    uniform_int_distribution<int> apple_y(5, 24);
-
-    keypad(stdscr, true); //возможность использовать клавиатуру
-    noecho(); //Отключает отображение нажимаемых клавиш;
-
-    while (!game_exit) {
-        rand_apple.x = apple_x(rnd);
-        rand_apple.y = apple_y(rnd);
-
-        showMap(apple, eaten_apples);
-        moveSnake(snake_head, vector_step, snake_tail, apple, rand_apple, eaten_apples);
-        timeout(frame_rate);
-
-        switch (getch()) {
-        case KEY_UP:
-            if (vector_step.x == 0) {
-                vector_step.x = -1;
-                vector_step.y = 0;
-                frame_rate = 170;
-            
-            }
-            break;
-        case KEY_DOWN:
-            if (vector_step.x == 0) {
-                vector_step.x = 1;
-                vector_step.y = 0;
-                frame_rate = 170;
-            }
-            break;
-        case KEY_LEFT:
-            if (vector_step.y == 0) {
-                vector_step.y = -1;
-                vector_step.x = 0;
-                frame_rate = 170;
-
-            }
-            break;
-        case KEY_RIGHT:
-            if (vector_step.y == 0) {
-                vector_step.y = 1;
-                vector_step.x = 0;
-                frame_rate = 170;
-            }
-            break;
-        case 'q':
-            game_exit = true;
-            break;
-        }
-    }
-    endwin(); 
 }
 
